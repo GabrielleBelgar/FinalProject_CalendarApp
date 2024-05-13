1a6d9adad29dc288bf2af247ae7e217188717624
charlante <144820692+charlante@users.noreply.github.com>
2024-05-14 03:29:49 +0800

    Final App.js

--- grp1_calendarapp/src/App.js
+++ grp1_calendarapp/src/App.js
@@ -8,48 +8,45 @@ import "react-big-calendar/lib/css/react-big-calendar.css";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import "./App.css";
+import ErrorModal from "./ErrorModal";
+import SuccessModal from "./SuccessModal";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faClock } from '@fortawesome/free-regular-svg-icons';
 import { faCalendarPlus } from '@fortawesome/free-regular-svg-icons';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
 
-
-
-
-
-
 const locales = {
-    "en-US": require("date-fns/locale/en-US"),
+  "en-US": require("date-fns/locale/en-US"),
 };
 const localizer = dateFnsLocalizer({
-    format,
-    parse,
-    startOfWeek,
-    getDay,
-    locales,
+  format: (date, formatStr, culture) => format(date, formatStr, { locale: locales[culture] }),
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
 });
 
 // DUMMY DATA
 const events = [
-    {
-        id: 0,
-        title: "Big Meeting",
-        allDay: true,
-        start: new Date(2021, 6, 0),
-        end: new Date(2021, 6, 0),
-    },
-    {
-        id: 1,
-        title: "Vacation",
-        start: new Date(2021, 6, 7),
-        end: new Date(2021, 6, 10),
-    },
-    {
-        id: 2,
-        title: "Conference",
-        start: new Date(2021, 6, 20),
-        end: new Date(2021, 6, 23),
-    },
+  {
+    id: 0,
+    title: "Big Meeting",
+    allDay: true,
+    start: new Date(2021, 6, 0),
+    end: new Date(2021, 6, 0),
+  },
+  {
+    id: 1,
+    title: "Vacation",
+    start: new Date(2021, 6, 7),
+    end: new Date(2021, 6, 10),
+  },
+  {
+    id: 2,
+    title: "Conference",
+    start: new Date(2021, 6, 20),
+    end: new Date(2021, 6, 23),
+  },
 ];
 
 const Popup = ({ event, onDelete, showPopup, setShowPopup, onEdit }) => {
@@ -75,7 +72,6 @@ const Popup = ({ event, onDelete, showPopup, setShowPopup, onEdit }) => {
   const handleDelete = () => {
     onDelete(event.id);
     setShowPopup(false);
-    alert("Event has been successfully deleted!");
   };
 
   const handleClose = () => {
@@ -160,126 +156,139 @@ const Popup = ({ event, onDelete, showPopup, setShowPopup, onEdit }) => {
   );
 };
 
+
 function App() {
-    const [newEvent, setNewEvent] = useState({ id: Date.now(), title: "", start: "", end: "" });
-    const [allEvents, setAllEvents] = useState(events);
-    const [showPopup, setShowPopup] = useState(false);
-    const [popupEvent, setPopupEvent] = useState(null);     
+  const [newEvent, setNewEvent] = useState({ id: Date.now(), title: "", start: "", end: "" });
+  const [allEvents, setAllEvents] = useState(events);
+  const [showPopup, setShowPopup] = useState(false);
+  const [popupEvent, setPopupEvent] = useState(null);
+  const [errorModalOpen, setErrorModalOpen] = useState(false);
+  const [successModalOpen, setSuccessModalOpen] = useState(false);
+  const [modalMessage, setModalMessage] = useState("");
 
-    function handleAddEvent() {
-        
-         // Check if the newEvent object contains valid data
+  const handleAddEvent = () => {
     if (!newEvent.title || !newEvent.start || !newEvent.end) {
-      alert("Please fill in all fields to add an event.");
+      setModalMessage("Please fill in all fields to add an event.");
+      setErrorModalOpen(true);
       return;
-  }
+    }
 
-  for (let i = 0; i < allEvents.length; i++) {
+    for (let i = 0; i < allEvents.length; i++) {
       const d1 = new Date(allEvents[i].start);
       const d2 = new Date(newEvent.start);
       const d3 = new Date(allEvents[i].end);
       const d4 = new Date(newEvent.end);
 
       if (((d1 <= d2) && (d2 <= d3)) || ((d1 <= d4) && (d4 <= d3))) {
-          alert("Event time overlaps with an existing event.");
-          return;
+        setModalMessage("Event time overlaps with an existing event.");
+        setErrorModalOpen(true);
+        return;
       }
-  }
+    }
 
     setAllEvents([...allEvents, newEvent]);
-    alert("Event has been successfully added!");
+    setSuccessModalOpen(true);
+    setModalMessage("Event has been successfully added!");
+  };
 
+  const handleDeleteEvent = (id) => {
+    const updatedEvents = allEvents.filter((event) => event.id !== id);
+    setAllEvents(updatedEvents);
+    setSuccessModalOpen(true);
+    setModalMessage("Event has been successfully deleted!");
+  };
 
-    }
+  const handleEditEvent = (editedEvent) => {
+    const updatedEvents = allEvents.map((event) =>
+      event.id === editedEvent.id ? editedEvent : event
+    );
+    setAllEvents(updatedEvents);
+    setShowPopup(false);
+    setSuccessModalOpen(true);
+    setModalMessage("Event has been successfully edited!");
+  };
 
-      const handleDeleteEvent = (id) => {
-        const updatedEvents = allEvents.filter((event) => event.id !== id);
-        setAllEvents(updatedEvents);
-        setShowPopup(false);
-      };
-    
-      function handleEditEvent(editedEvent) {
-        const updatedEvents = allEvents.map((event) =>
-          event.id === editedEvent.id ? editedEvent : event
-        );
-        setAllEvents(updatedEvents);
-        setShowPopup(false);
-        alert("Event has been successfully edited!");
-      }
-    
-      function handleEventClick(event) {
-        setPopupEvent(event);
-        setShowPopup(true);
-      }
-      
-    
-      return (
-        <div className="App">
-          <div className="Header">Event Scheduler</div>
-          <div className="AddEventGrp">
-          <h2 className="AddHeader">Add New Event</h2>
+  const handleEventClick = (event) => {
+    setPopupEvent(event);
+    setShowPopup(true);
+  };
 
-          <FontAwesomeIcon icon={faCalendarPlus} className="EventIcon" />
-            <input
-            className="EventTitle"
-              type="text"
-              placeholder="Add Title"
-              value={newEvent.title}
-              onChange={(e) =>
-                setNewEvent({ ...newEvent, title: e.target.value })
-              }
+  return (
+    <div className="App">
+      <div className="Header">Event Scheduler</div>
+      <div className="AddEventGrp">
+      <h2 className="AddHeader">Add New Event</h2>
+
+      <FontAwesomeIcon icon={faCalendarPlus} className="EventIcon" />
+        <input
+        className="EventTitle"
+          type="text"
+          placeholder="Add Title"
+          value={newEvent.title}
+          onChange={(e) =>
+            setNewEvent({ ...newEvent, title: e.target.value })
+          }
+        />
+        <br></br>
+        <div className="DateInput">
+            <FontAwesomeIcon icon={faClock} className="ClockIcon" />
+            <DatePicker
+              className="StartDate"
+              placeholderText="Start Date"
+              selected={newEvent.start}
+              onChange={(date) => setNewEvent({ ...newEvent, start: date })}
+              showTimeSelect
+              timeFormat="HH:mm"
+              timeIntervals={15}
+              dateFormat="MMMM d, yyyy h:mm aa"
+              timeCaption="Time"
             />
-            <br></br>
-            <div className="DateInput">
-                <FontAwesomeIcon icon={faClock} className="ClockIcon" />
-                <DatePicker
-                  className="StartDate"
-                  placeholderText="Start Date"
-                  selected={newEvent.start}
-                  onChange={(date) => setNewEvent({ ...newEvent, start: date })}
-                  showTimeSelect
-                  timeFormat="HH:mm"
-                  timeIntervals={15}
-                  dateFormat="MMMM d, yyyy h:mm aa"
-                  timeCaption="Time"
-                />
-                <DatePicker
-                  className="EndDate"
-                  placeholderText="End Date"
-                  selected={newEvent.end}
-                  onChange={(date) => setNewEvent({ ...newEvent, end: date })}
-                  showTimeSelect
-                  timeFormat="HH:mm"
-                  timeIntervals={15}
-                  dateFormat="MMMM d, yyyy h:mm aa"
-                  timeCaption="Time"
-                />
-              </div>
-                    <br></br>
-                  
-            <button className="submit-btn" onClick={handleAddEvent}>Add Event</button>
+            <DatePicker
+              className="EndDate"
+              placeholderText="End Date"
+              selected={newEvent.end}
+              onChange={(date) => setNewEvent({ ...newEvent, end: date })}
+              showTimeSelect
+              timeFormat="HH:mm"
+              timeIntervals={15}
+              dateFormat="MMMM d, yyyy h:mm aa"
+              timeCaption="Time"
+            />
+          </div>
+                <br></br>
+                <button className="submit-btn" onClick={handleAddEvent}>Add Event</button>
           </div>
           <div className="Calendar">
-            <Calendar
-              localizer={localizer}
-              events={allEvents}
-              startAccessor="start"
-              endAccessor="end"
-              onSelectEvent={handleEventClick}
-              selectable={true}
-              views={["month", "week", "day"]}
-              style={{ height: 800, margin: "50px"}}
-            />
-          </div>
-          <Popup
-            event={popupEvent}
-            onDelete={handleDeleteEvent}
-            onEdit={handleEditEvent}
-            showPopup={showPopup}
-            setShowPopup={setShowPopup}
-          />
-        </div>
-      );
-    }
-    
-    export default App;
+      <Calendar
+        localizer={localizer}
+        events={allEvents}
+        startAccessor="start"
+        endAccessor="end"
+        onSelectEvent={handleEventClick}
+        selectable={true}
+        views={["month", "week", "day"]}
+        style={{ height: 500, margin: "50px" }}
+      />
+      </div>
+      <Popup
+        event={popupEvent}
+        onDelete={handleDeleteEvent}
+        onEdit={handleEditEvent}
+        showPopup={showPopup}
+        setShowPopup={setShowPopup}
+      />
+      <ErrorModal
+        isOpen={errorModalOpen}
+        onClose={() => setErrorModalOpen(false)}
+        message={modalMessage}
+      />
+      <SuccessModal
+        isOpen={successModalOpen}
+        onClose={() => setSuccessModalOpen(false)}
+        message={modalMessage}
+      />
+    </div>
+  );
+}
+
+export default App;
